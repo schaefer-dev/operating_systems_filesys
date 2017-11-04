19b80f24544d31bc30d5c9232f88da55b7f788e1
Daniel Schaefer <schaefer.dev@gmail.com>
2017-11-04 12:34:45 +0100

    narrow bridge problem started

--- src/tests/threads/narrow-bridge.c
+++ src/tests/threads/narrow-bridge.c
@@ -36,7 +36,9 @@ void test_narrow_bridge(void)
 // maximal possible number of vehicles on the bridge at the same time
 int max_bridge_capacity = 3;
 // current number of vehicles on the bridge
-int vehicles_on_bridge=0;
+int vehicles_on_bridge = 0;
+// direction of vehicles which are on the bridge
+int vehicles_on_bridge_dir = 0;
 // number of normal vehicles waiting on the left hand side of the bridge
 int waiting_left = 0;
 // number of emergency vehicles waiting on the left hand side of the bridge
@@ -47,7 +49,7 @@ int waiting_right = 0;
 int waiting_emergency_right = 0;
 
 // semaphore to update variables safely
-struct semaphore lock;
+struct semaphore mutex;
 // semaphore to indicate that a vehicle on the left can drive
 struct semaphore vehicle_left;
 // semaphore to indicate that a emergency vehicle on the left can drive
@@ -56,14 +58,46 @@ struct semaphore emergency_left;
 struct semaphore vehicle_right;
 // semaphore to indicate that a emergency vehicle on the right can drive
 struct semaphore emergency_right;
+// simulated condition variable
+struct semaphore cars_can_drive;
+struct 
 
 /*TODO: add a variable (probably bool) to indicate the turn e.g. "right_turn" */
 
 void narrow_bridge(UNUSED unsigned int num_vehicles_left, UNUSED unsigned int num_vehicles_right,
         UNUSED unsigned int num_emergency_left, UNUSED unsigned int num_emergency_right)
 {
-    msg("NOT IMPLEMENTED");
-    /* FIXME implement */
+  waiting_left = num_vehicles_left;
+  waiting_right = num_vehicles_right;
+  waiting_emergency_left = num_emergency_left;
+  waiting_emergency_right = num_emergency_right;
+  
+  sema_init(&vehicle_left,3);
+  sema_init(&vehicle_right,3);
+  sema_init(&emergeny_left,3);
+  sema_init(&emergency_right,3);
+  sema_init(&mutex,1);
+
+  // TODO
 }
 
+void OneVehicle(int direc, int prio){
+  ArriveBridge(direc, prio);
+  CrossBridge(direc, prio);
+  ExitBridge(direc, prio);
+}
 
+ArriveBridge_car(unsigned int direc){
+  sema_down(mutex);
+
+}
+
+void ArriveBridge(unsigned int direc, unsigned int prio){
+  // TODO
+}
+void CrossBridge(unsigned int direc, unsigned int prio){
+  // TODO
+}
+void ExitBridge(unsigned int direc, unsigned int prio){
+  // TODO
+}
