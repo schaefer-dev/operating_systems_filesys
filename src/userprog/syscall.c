8c50c357d445d3f20d28a7601516ef37940c4df6
Daniel Schaefer <schaefer.dev@gmail.com>
2017-11-13 17:18:28 +0100

    first version of syscall switch

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -15,6 +15,83 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+  int syscall_type = *((int*)f->esp);
+
+  switch (syscall_type)
+    {
+    case SYS_HALT:
+      break;
+
+    case SYS_EXIT:
+      int exit_type = *((int*)f->esp + 1);
+      break;
+
+    case SYS_EXEC:
+      break;
+
+    case SYS_WAIT:
+      break;
+
+    case SYS_CREATE:
+      break;
+
+    case SYS_REMOVE:
+      break;
+
+    case SYS_OPEN:
+      break;
+
+    case SYS_FILESIZE:
+      break;
+
+    case SYS_READ:
+      break;
+
+    case SYS_WRITE:
+      break;
+
+    case SYS_SEEK:
+      break;
+
+    case SYS_TELL:
+      break;
+
+    case SYS_CLOSE:
+      break;
+
+    case SYS_MMAP:
+      // TODO project 3
+      break;
+
+    case SYS_MUNMAP:
+      // TODO project 3
+      break;
+
+    case SYS_CHDIR:
+      // TODO project 4
+      break;
+
+    case SYS_MKDIR:
+      // TODO project 4
+      break;
+
+    case SYS_READDIR:
+      // TODO project 4
+      break;
+
+    case SYS_ISDIR:
+      // TODO project 4
+      break;
+
+    case SYS_INUMBER:
+      // TODO project 4
+      break;
+
+    default:
+      printf("this should not happen! DEFAULT SYSCALL CASE");
+      break;
+    }
+  
   printf ("system call!\n");
   thread_exit ();
 }
