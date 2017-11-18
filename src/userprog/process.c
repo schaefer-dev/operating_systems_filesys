d041fb5da66521f15f04900d40716e6343a0b335
Daniel Schaefer <schaefer.dev@gmail.com>
2017-11-18 18:21:02 +0100

    some notes for process_execute

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -38,6 +38,9 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
+  // TODO calculate start_process
+  // thread_create is where the magic happens
+
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
